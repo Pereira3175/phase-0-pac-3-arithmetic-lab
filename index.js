0641c8e7421c35f152edeb3016d7d72f1839ba26
Luis <pereira1214@gmail.com>
2021-08-09 14:18:35 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+    return 1+2
+}
+function subtract() {
+    return 3-1
+}
+function multiply() {
+    return 2*5
+}
+function divide() {
+    return 25%5
+}
+function add(a, b) {
+    console.log (a)
+    console.log (b)
+    return a+b
+}
+function subtract(a, b) {
+    return a-b
+}
+function multiply(a, b) {
+    return a*b
+}
+function divide(a, b) {
+    return a/b
+}
+function increment(n) {
+    return n+=1
+}
+function decrement(n) {
+    return n-=1
+}
+function makeInt(n) {
+   return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n, '2.222')
+}
